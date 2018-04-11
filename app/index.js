6310ca6f0dd546430df05a403fb53f8ee351da77
Juanjo <jramirez@agbotraining.com>
2018-04-11 22:54:47 +0200

    Adds more endpoints for the rest of sensors

--- app/index.js
+++ app/index.js
@@ -26,7 +26,47 @@ MongoClient.connect(MONGO_URL).then(db => {
   app.get('/mcp3008', function(req, res) {
     dataCollection.find().sort({ date: -1 }).toArray()
     .then(values => res.json(values.map(x => ({
-	    date: x.date, mcp_0: x.mcp_0, mcp_1: x.mcp_1, mcp_2: x.mcp_2, mcp_3: x.mcp_3, mcp_4: x.mcp_4, mcp_5: x.mcp_5, mcp_6: x.mcp_6, mcp_7: x.mcp_7
+      date: x.date,
+      mcp_0: x.mcp_0,
+      mcp_1: x.mcp_1,
+      mcp_2: x.mcp_2,
+      mcp_3: x.mcp_3,
+      mcp_4: x.mcp_4,
+      mcp_5: x.mcp_5,
+      mcp_6: x.mcp_6,
+      mcp_7: x.mcp_7,
+    }))));
+  });
+
+  app.get('/light', function(req, res) {
+    dataCollection.find().sort({ date: -1 }).toArray()
+    .then(values => res.json(values.map(x => ({
+      date: x.date,
+      light: x.light,
+    }))));
+  });
+
+  app.get('/temp1w', function(req, res) {
+    dataCollection.find().sort({ date: -1 }).toArray()
+    .then(values => res.json(values.map(x => ({
+      date: x.date,
+      temperature: x.temp1w,
+    }))));
+  });
+
+  app.get('/dht22_humidity', function(req, res) {
+    dataCollection.find().sort({ date: -1 }).toArray()
+    .then(values => res.json(values.map(x => ({
+      date: x.date,
+      humidity: x.dht22_humidity,
+    }))));
+  });
+
+  app.get('/dht22_temperature', function(req, res) {
+    dataCollection.find().sort({ date: -1 }).toArray()
+    .then(values => res.json(values.map(x => ({
+      date: x.date,
+      temperature: x.dht22_temperature,
     }))));
   });
 
